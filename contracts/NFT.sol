878821b22ce1854a55044a4e2ef88a20a5ff8da3
whitewizardd <o.samuel@native.semicolon.africa>
2024-02-26 19:26:52 +0100

    updated

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract VideoNFTMarketplace is ERC721 {
+contract NFTMarketPlace is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
